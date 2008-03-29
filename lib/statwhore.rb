a07ededead49c972ea880d1930747e62c1009203
John Nunemaker <nunemaker@gmail.com>
2008-03-28 22:49:06 -0400

    added some of the dependencies that i forgot to do initially

--- lib/statwhore.rb
+++ lib/statwhore.rb
@@ -5,9 +5,10 @@ require 'net/https'
 require 'uri'
 require 'ostruct'
 require 'rubygems'
+gem 'hpricot', '>= 0.6'
 require 'hpricot'
+gem 'active_support', '>= 2.0.2'
 require 'active_support'
-
 gem 'googlebase', '>= 0.2.0'
 require 'google/base'
 
